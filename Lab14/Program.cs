4661dcd221aa328924919c34929a0a3760ae67b1
Надежда Кузнецова <kuznecovanadezhd@gmail.com>
2022-01-13 21:05:23 +0300

    Добавьте файлы проекта.

---
+++ Lab14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Алиса");
+            cat.ShowInfo();
+            Dog dog = new Dog("Арчи");
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
